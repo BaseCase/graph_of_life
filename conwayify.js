3e79d43c30428e8079452397b8c36102944f8715
Casey Brant <casey@caseybrant.com>
2016-03-19 22:38:29 -0500

    fix one instance of " instead of ' so that it bookmarkletifies better

--- conwayify.js
+++ conwayify.js
@@ -25,7 +25,7 @@ javascript:(function() {
     /* ignore first and last cols, since they might be different heights */
     return [].map.call(svg_columns.slice(1, svg_columns.length - 1), function(col) {
       return [].map.call(col.children, function(node) {
-        var is_alive = node.getAttribute('fill') !== "#eeeeee";
+        var is_alive = node.getAttribute('fill') !== '#eeeeee';
         return new Cell(is_alive, node);
       });
     });
